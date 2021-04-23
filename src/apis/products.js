ddd231975a33f77021c22182055b4afa78eec5cf
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-23 09:53:37 -0700

    Create products.js

---
+++ src/apis/products.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/";
+
+const fetch = () => axios.get(`${baseUrl}api/v1/products`);
+
+const create = payload => axios.post(`${baseUrl}api/v1/products`, payload);
+
+const update = (id, payload) =>
+  axios.put(`${baseUrl}api/v1/products/${id}`, payload);
+
+const destroy = payload =>
+  axios.post(`${baseUrl}api/v1/products/bulk_delete`, payload);
+
+const productsApi = {
+  fetch,
+  create,
+  update,
+  destroy
+};
+
+export default productsApi;
