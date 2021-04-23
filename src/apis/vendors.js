04a06f292ede23364570b86335ade6e420096c39
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-23 09:53:32 -0700

    Create vendors.js

---
+++ src/apis/vendors.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/";
+
+const fetch = () => axios.get(`${baseUrl}api/v1/vendors`);
+
+const create = payload => axios.post(`${baseUrl}api/v1/vendors`, payload);
+
+const update = (id, payload) =>
+  axios.put(`${baseUrl}api/v1/vendors/${id}`, payload);
+
+const destroy = payload =>
+  axios.post(`${baseUrl}api/v1/vendors/bulk_delete`, payload);
+
+const vendorsApi = {
+  fetch,
+  create,
+  update,
+  destroy
+};
+
+export default vendorsApi;
