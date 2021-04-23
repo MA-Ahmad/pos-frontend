4e71d12637d6c3f101f83d3e9d754ca93c4c1359
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-23 09:53:42 -0700

    Update axios.js

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,32 +1,34 @@
-import axios from 'axios';
+import react from "react";
+import axios from "axios";
 // import { Toastr } from "neetoui";
+import { useToast } from "@chakra-ui/react";
 
-axios.defaults.baseURL = '/';
+axios.defaults.baseURL = "/";
 
 export const setAuthHeaders = (setLoading = () => null) => {
   axios.defaults.headers = {
-    Accept: 'applicaion/json',
-    'Content-Type': 'application/json',
+    Accept: "applicaion/json",
+    "Content-Type": "application/json"
   };
-  const token = JSON.parse(localStorage.getItem('authToken'));
-  const email = JSON.parse(localStorage.getItem('authEmail'));
+  const token = JSON.parse(localStorage.getItem("authToken"));
+  const email = JSON.parse(localStorage.getItem("authEmail"));
   if (token && email) {
-    axios.defaults.headers['X-Auth-Email'] = email;
-    axios.defaults.headers['X-Auth-Token'] = token;
+    axios.defaults.headers["X-Auth-Email"] = email;
+    axios.defaults.headers["X-Auth-Token"] = token;
   }
   setLoading(false);
 };
 
 export const resetAuthTokens = () => {
-  delete axios.defaults.headers['X-Auth-Email'];
-  delete axios.defaults.headers['X-Auth-Token'];
+  delete axios.defaults.headers["X-Auth-Email"];
+  delete axios.defaults.headers["X-Auth-Token"];
 };
 
 const handleSuccessResponse = response => {
   if (response) {
     response.success = response.status === 200;
     if (response.data.notice) {
-      // Toastr.success(response.data.notice);
+      ShowToast();
     }
   }
   return response;
@@ -34,12 +36,22 @@ const handleSuccessResponse = response => {
 
 const handleErrorResponse = (error, authDispatch) => {
   if (error.response?.status === 401) {
-    authDispatch({ type: 'LOGOUT' });
-    // Toastr.error(error.response?.data?.error);
+    authDispatch({ type: "LOGOUT" });
+    ShowToast(error.response?.data?.error, "error");
   } else {
-    // Toastr.error(error.response?.data?.error || error.message);
+    ShowToast(error.response?.data?.error || error.message, "error");
   }
   return Promise.reject(error);
+};
+
+const ShowToast = (response, status) => {
+  const toast = useToast();
+  toast({
+    description: response,
+    status: status,
+    duration: 1500,
+    isClosable: true
+  });
 };
 
 export const registerIntercepts = authDispatch => {
