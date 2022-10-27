afd51460b47d2b3bb1e8274c25846f33a1bbd3d5
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-10-28 02:10:55 +0500

    Update baseUrl.js

--- src/apis/baseUrl.js
+++ src/apis/baseUrl.js
@@ -1,3 +1,4 @@
-export const baseUrl = "https://pos-backend-rails.herokuapp.com/";
+export const baseUrl = 'https://pos-backend-rails-app.herokuapp.com/'
+// export const baseUrl = "https://pos-backend-rails.herokuapp.com/";
 // export const baseUrl = "http://localhost:3001/";
 // export const baseUrl = 'https://auth-app-rails-back.herokuapp.com/';
